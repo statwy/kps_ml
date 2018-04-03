737f6ef351a0d2e1bcf3b1510b11510f6ffb4142
hychoi <hychoi508@gmail.com>
2018-04-03 10:33:28 +0900

    chy
    crawling함수에 savetohaoop연동

--- function/mile/savetohadoop.py
+++ function/mile/savetohadoop.py
@@ -5,19 +5,12 @@ Created on Mon Apr  2 16:03:30 2018
 @author: user
 """
 
-
 import os
 import subprocess
-from hdfs import InsecureClient
 
-def savetohadoop(filename):
+def savetofile(filename):
     os.chdir('/home/rabbit/hadoop2/')
     subprocess.call('./bin/hdfs dfs -ls /test', shell=True)
     file=filename
     subprocess.call('./bin/hdfs dfs -put /home/rabbit/data_anal/kps_ml/data/'+file+'.csv /user/hdfs/test', shell=True)
 
-def savetohadoop_d(dataframe,filename):
-    client_hdfs = InsecureClient('http://http://10.1.43.149:50070')
-    with client_hdfs.write('/user/hdfs/test/'+filename+'.csv', encoding = 'utf-8') as writer:
-        dataframe.to_csv(writer)                                
-       
