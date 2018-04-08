3c38d8e874d25bbb342e66dd20be367f4a239928
statwy <jwy627@naver.com>
2018-04-08 18:53:55 +0900

    wy
    comm

--- knn.py
+++ knn.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
 data_for_knn=pd.read_csv("data/knndata.csv")
+print(data_for_knn['premium'].groupby(data_for_knn['pre_class']).count())
 
 x = data_for_knn.iloc[:, 3:17]
 y = data_for_knn['pre_class']
@@ -31,7 +32,7 @@ predY = knn.predict(trainX)
 accuracy = 100 * (trainY == predY).sum() / len(predY)
 print("* 학습용 데이터로 측정한 정확도 = %.2f" % accuracy, '%')
 
-## k를 변화시켜가면서 정확도를 측정해 본다
+# k를 변화시켜가면서 정확도를 측정해 본다
 #testAcc = []
 #trainAcc = []
 #for k in range(5, 100):
@@ -42,11 +43,11 @@ print("* 학습용 데이터로 측정한 정확도 = %.2f" % accuracy, '%')
 #    # Test 세트의 Feature에 대한 정확도
 #    predY = knn.predict(testX)
 #    testAcc.append((testY == predY).sum() / len(predY))
-#    
+#    print('testAcc:',testAcc,'K:',k)
 #    # Train 세트의 Feature에 대한 정확도
 #    predY = knn.predict(trainX)
 #    trainAcc.append((trainY == predY).sum() / len(predY))
-
+#    print('trainAcc:',trainAcc,'K:',k)
 #plt.figure(figsize=(8, 5))
 #plt.plot(testAcc, label="Test Data")
 #plt.plot(trainAcc, label="Train Data")
