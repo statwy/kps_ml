3c76c98932ee29677985bc5f95281b84b6e5bd10
hychoi <hychoi508@gmail.com>
2018-04-11 21:53:45 +0900

    commit
    commit

--- function/kmeans_lstm_model.py
+++ function/kmeans_lstm_model.py
@@ -48,7 +48,7 @@ def Kmeans_LSTM(data) :
     model.add(LSTM(256, input_shape=(nPrior,k)))
     model.add(Dense(one_hot_vec_size, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
-    history = model.fit(trainX, trainY, batch_size=5, epochs =10)
+    history = model.fit(trainX, trainY, batch_size=10, epochs =100)
     
     print("3")
     
